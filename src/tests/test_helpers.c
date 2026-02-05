88551ce0c3373ebb5436dbbff9484feae19ad88d
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2026-02-06 02:54:41 +0300

    test(is_zero): add tests for s21_is_zero

---
+++ src/tests/test_helpers.c
@@ -0,0 +1,71 @@
+#include <check.h>
+#include "../headers/s21_helpers.h"
+
+
+
+START_TEST(s21_is_zero_first_bit_is_digit){
+    s21_big_decimal val = {{21, 0, 0, 0, 0, 0, 0, 0}};
+    ck_assert_int_eq(s21_is_zero(val), 0);
+}
+END_TEST
+
+START_TEST(s21_is_zero_second_bit_is_digit){
+    s21_big_decimal val = {{0, 40, 0, 0, 0, 0, 0, 0}};
+    ck_assert_int_eq(s21_is_zero(val), 0);
+}
+END_TEST
+
+START_TEST(s21_is_zero_third_bit_is_digit){
+    s21_big_decimal val = {{0, 0, 30, 0, 0, 0, 0, 0}};
+    ck_assert_int_eq(s21_is_zero(val), 0);
+}
+END_TEST
+
+
+START_TEST(s21_is_zero_fourth_bit_is_digit){
+    s21_big_decimal val = {{0, 0, 0, 40, 0, 0, 0, 0}};
+    ck_assert_int_eq(s21_is_zero(val), 1);
+}
+END_TEST
+
+START_TEST(s21_is_zero_all_zero) {
+    s21_big_decimal val = {{0, 0, 0, 0, 0, 0, 0, 0}};
+    ck_assert_int_eq(s21_is_zero(val), 1);
+}
+END_TEST
+
+START_TEST(s21_is_zero_multiple_nonzero) {
+    s21_big_decimal val = {{1, 2, 0, 0, 0, 0, 0, 0}};
+    ck_assert_int_eq(s21_is_zero(val), 0);
+}
+END_TEST
+
+START_TEST(s21_is_zero_last_bit_nonzero) {
+    s21_big_decimal val = {{0, 0, 0, 0, 0, 0, 0, 5}};
+    ck_assert_int_eq(s21_is_zero(val), 1);
+}
+END_TEST
+
+
+Suite* s21_helpers_suite(void) {
+    Suite *s = suite_create("helpers");
+    TCase *tc_core = tcase_create("Core");
+    tcase_add_test(tc_core, s21_is_zero_first_bit_is_digit);
+    tcase_add_test(tc_core, s21_is_zero_second_bit_is_digit);
+    tcase_add_test(tc_core, s21_is_zero_third_bit_is_digit);
+    tcase_add_test(tc_core, s21_is_zero_fourth_bit_is_digit);
+    tcase_add_test(tc_core, s21_is_zero_all_zero);
+    tcase_add_test(tc_core, s21_is_zero_multiple_nonzero);
+    tcase_add_test(tc_core, s21_is_zero_last_bit_nonzero);
+    suite_add_tcase(s, tc_core);
+    return s;
+}
+
+int main(void) {
+    Suite *s = s21_helpers_suite();
+    SRunner *sr = srunner_create(s);
+    srunner_run_all(sr, CK_NORMAL);
+    int failed = srunner_ntests_failed(sr);
+    srunner_free(sr);
+    return (failed == 0) ? 0 : 1;
+}
