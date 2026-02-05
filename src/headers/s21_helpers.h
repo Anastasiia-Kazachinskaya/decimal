dcb98c39692780ab8d57c7e605ee87f66db04dfe
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2026-02-06 02:42:50 +0300

    chore(headers.helpers): todo -> done, fix include <> -> ""

--- src/headers/s21_helpers.h
+++ src/headers/s21_helpers.h
@@ -1,7 +1,7 @@
 #ifndef S21_HELPERS_H
 #define S21_HELPERS_H
 
-#include <s21_decimal.h>
+#include "s21_decimal.h"
 
 typedef struct {
   unsigned bits[8];
@@ -15,7 +15,7 @@ int s21_get_scale(s21_decimal value);                                // TODO
 int s21_set_scale(s21_decimal* value, int scale);                    // TODO
 int s21_get_bit(s21_decimal value, int bit_index);                   // TODO
 int s21_set_bit(s21_decimal* value, int bit_index, int bit_value);   // TODO
-int s21_is_zero(s21_decimal value);                                  // TODO
+int s21_is_zero(s21_big_decimal value);                              // DONE
 int s21_null_decimal(s21_decimal value);                             // TODO
 int s21_normalize_pair(s21_decimal* value_1, s21_decimal* value_2);  // TODO
 int s21_shift_left(s21_big_decimal* value, int shift);               // TODO
