c117f76918df2f311468ad71f7c9663ce79ce576
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2026-02-06 17:15:17 +0300

    hot_fix(test_helpers): fixed the tests to work with Decimal type

--- src/tests/test_helpers.c
+++ src/tests/test_helpers.c
@@ -4,47 +4,43 @@
 
 
 START_TEST(s21_is_zero_first_bit_is_digit){
-    s21_big_decimal val = {{21, 0, 0, 0, 0, 0, 0, 0}};
+    s21_decimal val = {{21, 0, 0, 0}};
     ck_assert_int_eq(s21_is_zero(val), 0);
 }
 END_TEST
 
 START_TEST(s21_is_zero_second_bit_is_digit){
-    s21_big_decimal val = {{0, 40, 0, 0, 0, 0, 0, 0}};
+    s21_decimal val = {{0, 40, 0, 0}};
     ck_assert_int_eq(s21_is_zero(val), 0);
 }
 END_TEST
 
 START_TEST(s21_is_zero_third_bit_is_digit){
-    s21_big_decimal val = {{0, 0, 30, 0, 0, 0, 0, 0}};
+    s21_decimal val = {{0, 0, 30, 0}};
     ck_assert_int_eq(s21_is_zero(val), 0);
 }
 END_TEST
 
 
 START_TEST(s21_is_zero_fourth_bit_is_digit){
-    s21_big_decimal val = {{0, 0, 0, 40, 0, 0, 0, 0}};
+    s21_decimal val = {{0, 0, 0, 40}};
     ck_assert_int_eq(s21_is_zero(val), 1);
 }
 END_TEST
 
 START_TEST(s21_is_zero_all_zero) {
-    s21_big_decimal val = {{0, 0, 0, 0, 0, 0, 0, 0}};
+    s21_decimal val = {{0, 0, 0, 0}};
     ck_assert_int_eq(s21_is_zero(val), 1);
 }
 END_TEST
 
 START_TEST(s21_is_zero_multiple_nonzero) {
-    s21_big_decimal val = {{1, 2, 0, 0, 0, 0, 0, 0}};
+    s21_decimal val = {{1, 2, 0, 0}};
     ck_assert_int_eq(s21_is_zero(val), 0);
 }
 END_TEST
 
-START_TEST(s21_is_zero_last_bit_nonzero) {
-    s21_big_decimal val = {{0, 0, 0, 0, 0, 0, 0, 5}};
-    ck_assert_int_eq(s21_is_zero(val), 1);
-}
-END_TEST
+
 
 
 Suite* s21_helpers_suite(void) {
@@ -56,7 +52,7 @@ Suite* s21_helpers_suite(void) {
     tcase_add_test(tc_core, s21_is_zero_fourth_bit_is_digit);
     tcase_add_test(tc_core, s21_is_zero_all_zero);
     tcase_add_test(tc_core, s21_is_zero_multiple_nonzero);
-    tcase_add_test(tc_core, s21_is_zero_last_bit_nonzero);
+
     suite_add_tcase(s, tc_core);
     return s;
 }
