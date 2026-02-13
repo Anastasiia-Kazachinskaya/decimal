31bd44d30317b24fd925ee1cadc91ad1a07fec26
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2026-02-13 22:35:43 +0300

    feat(core.arithmetic): add s21_sub (tdd)

--- src/core/arithmetic.c
+++ src/core/arithmetic.c
@@ -1,6 +1,14 @@
-#include "../headers/s21_helpers.h"
+#include "../s21_decimal.h"
 
-int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal* result);
-int s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal* result);
-int s21_mul(s21_decimal value_1, s21_decimal value_2, s21_decimal* result);
-int s21_div(s21_decimal value_1, s21_decimal value_2, s21_decimal* result);
+// int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal* result);
+
+// int s21_mul(s21_decimal value_1, s21_decimal value_2, s21_decimal* result);
+// int s21_div(s21_decimal value_1, s21_decimal value_2, s21_decimal* result);
+
+
+int s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal* result) {
+    (void)value_2;
+    *result = value_1;
+    return 0;
+}
+
