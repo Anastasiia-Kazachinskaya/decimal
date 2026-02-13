5ef2b234f311be2f707d89fbbc8188db91b5ca94
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2026-02-13 22:34:44 +0300

    test(core.arithmetic): add s21_sub_zero

--- src/tests/test_arithmetic.c
+++ src/tests/test_arithmetic.c
@@ -5,3 +5,29 @@
 #include <stdlib.h>
 
 #include "../s21_decimal.h"
+
+// s21_sub tests section
+START_TEST(s21_sub_zero){
+    int status;
+    s21_decimal result = {{0, 0, 0, 0}};
+    s21_decimal value_1 = {{5, 0, 0, 0}};
+    s21_decimal value_2 = {{0, 0, 0, 0}};
+    status = s21_sub(value_1, value_2, &result);
+    ck_assert_int_eq(result.bits[0], 5);
+    ck_assert_int_eq(result.bits[1], 0);
+    ck_assert_int_eq(result.bits[2], 0);
+    ck_assert_int_eq(result.bits[3], 0);
+    ck_assert_int_eq(status, 0);
+}
+END_TEST
+
+
+Suite *s21_arithmetic_suite(void) {
+    Suite *s = suite_create("srithmetic");
+    TCase *tc_core = tcase_create("Core");
+    tcase_add_test(tc_core, s21_sub_zero);
+
+    suite_add_tcase(s, tc_core);
+    return s;
+}
+
