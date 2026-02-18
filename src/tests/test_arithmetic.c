cbcf6bbd28dead281340a3d42ef58d074e48c47b
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2026-02-18 18:13:35 +0300

    test: add s21_sub_positive_minus_negative

--- src/tests/test_arithmetic.c
+++ src/tests/test_arithmetic.c
@@ -88,6 +88,23 @@ START_TEST(s21_sub_negative_minus_positive){
 }
 END_TEST
 
+START_TEST(s21_sub_positive_minus_negative){
+    int status;
+    s21_decimal result = {{0, 0, 0, 0}};
+    s21_decimal value_1 = {{2, 0, 0, 0}};
+    s21_decimal value_2 = {{5, 0, 0, 0}};
+
+    value_2.bits[3] |= 1u << 31;
+    
+    status = s21_sub(value_1, value_2, &result);
+    ck_assert_int_eq(result.bits[0], 7);
+    ck_assert_int_eq(result.bits[1], 0);
+    ck_assert_int_eq(result.bits[2], 0);
+    ck_assert_int_eq(s21_get_sign(&result), 0);
+    ck_assert_int_eq(status, 0);
+}
+END_TEST
+
 Suite *s21_arithmetic_suite(void) {
     Suite *s = suite_create("arithmetic");
     TCase *tc_core = tcase_create("Core");
@@ -96,7 +113,7 @@ Suite *s21_arithmetic_suite(void) {
     tcase_add_test(tc_core, s21_sub_positive_minus_larger_positive);
     tcase_add_test(tc_core, s21_sub_negative_minus_negative);
     tcase_add_test(tc_core, s21_sub_negative_minus_positive);
-
+    tcase_add_test(tc_core, s21_sub_positive_minus_negative);
 
     suite_add_tcase(s, tc_core);
     return s;
