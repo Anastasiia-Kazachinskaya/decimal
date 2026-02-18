aaaa620691c0758473b213356182f1a9ba76e431
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2026-02-18 16:27:17 +0300

    add s21_sub_positive_minus_larger_positive

--- src/tests/test_arithmetic.c
+++ src/tests/test_arithmetic.c
@@ -5,9 +5,10 @@
 #include <stdlib.h>
 
 #include "../s21_decimal.h"
+#include "../headers/s21_helpers.h"
 
 // s21_sub tests section
-START_TEST(s21_sub_zero){
+START_TEST(s21_sub_positive_minus_zero){
     int status;
     s21_decimal result = {{0, 0, 0, 0}};
     s21_decimal value_1 = {{5, 0, 0, 0}};
@@ -22,7 +23,7 @@ START_TEST(s21_sub_zero){
 END_TEST
 
 
-START_TEST(s21_sub_positive_no_scale_int){
+START_TEST(s21_sub_positive_minus_lower_positive){
     int status;
     s21_decimal result = {{0, 0, 0, 0}};
     s21_decimal value_1 = {{5, 0, 0, 0}};
@@ -37,11 +38,28 @@ START_TEST(s21_sub_positive_no_scale_int){
 END_TEST
 
 
+START_TEST(s21_sub_positive_minus_larger_positive){
+    int status;
+    s21_decimal result = {{0, 0, 0, 0}};
+    s21_decimal value_1 = {{2, 0, 0, 0}};
+    s21_decimal value_2 = {{5, 0, 0, 0}};
+    status = s21_sub(value_1, value_2, &result);
+    ck_assert_int_eq(result.bits[0], 3);
+    ck_assert_int_eq(result.bits[1], 0);
+    ck_assert_int_eq(result.bits[2], 0);
+    ck_assert_int_eq(s21_get_sign(&result), 1);
+    ck_assert_int_eq(status, 0);
+}
+END_TEST
+
+
 Suite *s21_arithmetic_suite(void) {
     Suite *s = suite_create("arithmetic");
     TCase *tc_core = tcase_create("Core");
-    tcase_add_test(tc_core, s21_sub_zero);
-    tcase_add_test(tc_core, s21_sub_positive_no_scale_int);
+    tcase_add_test(tc_core, s21_sub_positive_minus_zero);
+    tcase_add_test(tc_core, s21_sub_positive_minus_lower_positive);
+    tcase_add_test(tc_core, s21_sub_positive_minus_larger_positive);
+
 
     suite_add_tcase(s, tc_core);
     return s;
