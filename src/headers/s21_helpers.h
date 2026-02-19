1e616ba297370c99795b5bd94e1bd8217878aacc
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2026-02-19 22:26:10 +0300

    feat(headers.helpers): declare s21_big_sub, s21_big_add

--- src/headers/s21_helpers.h
+++ src/headers/s21_helpers.h
@@ -18,6 +18,10 @@ typedef struct {
 
 s21_big_decimal s21_decimal_to_big(s21_decimal *value);               // DO
 s21_decimal s21_big_to_decimal(s21_big_decimal* big_result, s21_decimal* result);               // TODO
+int s21_big_sub(s21_big_decimal value_1, s21_big_decimal value_2,
+            s21_big_decimal* result);  // TODO
+int s21_big_add(s21_big_decimal value_1, s21_big_decimal value_2,
+            s21_big_decimal* result);  // TODO
 int s21_is_big_less(s21_big_decimal value1, s21_big_decimal value2);
 int s21_is_big_greater(s21_big_decimal value1, s21_big_decimal value2);
 int s21_get_sign(s21_decimal *value);                                 // DO
