a1044d43ac7ca70c812f21606f05130113991e33
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2026-02-19 22:23:53 +0300

    feat(core.compare): add template s21_is_big_less, s21_is_big_greater

--- src/core/comparison.c
+++ src/core/comparison.c
@@ -6,3 +6,17 @@ int s21_is_greater(s21_decimal, s21_decimal);
 int s21_is_greater_or_equal(s21_decimal, s21_decimal);
 int s21_is_less(s21_decimal, s21_decimal);
 int s21_is_less_or_equal(s21_decimal, s21_decimal);
+
+
+int s21_is_big_less(s21_big_decimal value1, s21_big_decimal value2) {
+    (void) value1;
+    (void) value2;
+    return 0;
+}
+
+int s21_is_big_greater(s21_big_decimal value1, s21_big_decimal value2) {
+    (void) value1;
+    (void) value2;
+    return 0;
+}
+
