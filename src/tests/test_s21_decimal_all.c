42ae07f6fdecd85625ad857a0e2e3855ce0560b4
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2026-02-19 22:54:17 +0300

    test(decimal_all): add s21_comparison_suite

--- src/tests/test_s21_decimal_all.c
+++ src/tests/test_s21_decimal_all.c
@@ -4,13 +4,15 @@
 
 Suite* s21_helpers_suite(void);
 Suite *s21_arithmetic_suite(void);
+Suite *s21_comparison_suite(void);
 
 int main(void) {
     int failed = 0;
     
     Suite* suites[] = {
         s21_helpers_suite(),
-        // s21_arithmetic_suite(),
+        s21_arithmetic_suite(),
+        s21_comparison_suite(),
         NULL
     };
     
