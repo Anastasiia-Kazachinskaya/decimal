c5524573a7f12138446ca9727518601e343b4fc8
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2026-02-20 18:51:28 +0300

    test(arith): add s21_sub_positive_minus_lower_positive

--- src/tests/test_arithmetic.c
+++ src/tests/test_arithmetic.c
@@ -28,25 +28,31 @@ START_TEST(s21_sub_positive_minus_zero){
 }
 END_TEST
 
-/*
 
-TODO: s21_big_to_decimal TEST
+
+// TODO: s21_big_to_decimal TEST
 
 
 START_TEST(s21_sub_positive_minus_lower_positive){
     int status;
-    s21_decimal result = {{0, 0, 0, 0}};
+
     s21_decimal value_1 = {{5, 0, 0, 0}};
     s21_decimal value_2 = {{3, 0, 0, 0}};
-    status = s21_sub(value_1, value_2, &result);
-    ck_assert_int_eq(result.bits[0], 2);
-    ck_assert_int_eq(result.bits[1], 0);
-    ck_assert_int_eq(result.bits[2], 0);
-    ck_assert_int_eq(result.bits[3], 0);
-    ck_assert_int_eq(status, 0);
+    s21_big_decimal five = s21_decimal_to_big(&value_1);
+    s21_big_decimal three = s21_decimal_to_big(&value_2);
+    s21_big_decimal big_result;
+    s21_null_big_decimal(&big_result);
+
+    status = s21_big_sub(five, three, &big_result);
+
+    ck_assert_int_eq(big_result.bits[0], 2);
+    ck_assert_int_eq(big_result.bits[1], 0);
+    ck_assert_int_eq(big_result.bits[2], 0);
+    ck_assert_int_eq(big_result.bits[6], 0);
+    ck_assert_int_eq(status, OK);
 }
 END_TEST
-
+/*
 
 START_TEST(s21_sub_positive_minus_larger_positive){
     int status;
@@ -119,7 +125,7 @@ Suite *s21_arithmetic_suite(void) {
     Suite *s = suite_create("arithmetic");
     TCase *tc_core = tcase_create("Core");
     tcase_add_test(tc_core, s21_sub_positive_minus_zero);
-    // tcase_add_test(tc_core, s21_sub_positive_minus_lower_positive);
+    tcase_add_test(tc_core, s21_sub_positive_minus_lower_positive);
     // tcase_add_test(tc_core, s21_sub_positive_minus_larger_positive);
     // tcase_add_test(tc_core, s21_sub_negative_minus_negative);
     // tcase_add_test(tc_core, s21_sub_negative_minus_positive);
