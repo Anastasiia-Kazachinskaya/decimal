49b987fbdd5a33a48e6670cb7cdadd977f9bc488
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2026-02-20 15:48:04 +0300

    feat(core.comparison): add s21_is_big_greater

--- src/core/comparison.c
+++ src/core/comparison.c
@@ -15,8 +15,18 @@ int s21_is_big_less(s21_big_decimal value1, s21_big_decimal value2) {
 }
 
 int s21_is_big_greater(s21_big_decimal value1, s21_big_decimal value2) {
-    (void) value1;
-    (void) value2;
-    return 0;
+    int result = 0;
+    int found_diff = 0;
+    for (int i = 6; i >= 0 && (!found_diff); i--) {
+        if (value1.bits[i] > value2.bits[i]) {
+            found_diff = 1;
+            result = 1;
+        } else if (value1.bits[i] < value2.bits[i]) {
+            found_diff = 1;
+            result = 0;
+        }
+    }
+
+    return result;
 }
 
