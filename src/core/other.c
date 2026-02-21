8560b3bfc1b691cee7cd7bfc24112d18b9e20185
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2026-02-21 14:28:09 +0300

    feat(core.other): add s21_get_overflow

--- src/core/other.c
+++ src/core/other.c
@@ -1,4 +1,6 @@
+#include <stdint.h> 
 #include "../headers/s21_helpers.h"
+
 
 int s21_floor(s21_decimal value, s21_decimal* result);
 int s21_negate(s21_decimal value, s21_decimal* result);
@@ -18,4 +20,17 @@ int s21_normalize_pair(s21_decimal* value_1, s21_decimal* value_2) {
     (void) value_1;
     (void) value_2;
     return 0;
+}
+
+int s21_get_overflow(s21_big_decimal* value) {
+    int overflow = 0;
+    for (int i = 0; i < S21_DECIMAL_LIMIT; i++) {
+        value->bits[i] += overflow;
+        overflow = (uint64_t)value->bits[i] >> 32;
+        value->bits[i] &= MAX4BITE;
+    }
+    if(overflow) {
+            return ERROR; // 1
+        }
+    return OK; // 0
 }
