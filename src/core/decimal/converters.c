0c434de178303db681678ff984923f9299bb88da
titanlig <kattyalex97@mail.ru>
2026-03-19 00:55:31 +0300

    add conv

--- src/core/decimal/converters.c
+++ src/core/decimal/converters.c
@@ -1,5 +1,5 @@
-#include "../../headers/s21_utils.h"
 #include "../../headers/s21_big_decimal.h"
+#include "../../headers/s21_utils.h"
 #include "../../s21_decimal.h"
 
 int s21_from_decimal_to_float(s21_decimal src, float* dst) {
@@ -26,21 +26,20 @@ int s21_from_decimal_to_float(s21_decimal src, float* dst) {
 int s21_from_decimal_to_int(s21_decimal src, int* dst) {
   int res = CONVERTATION_ERROR;
   if (!dst) return res;
+  if (s21_decimal_check(src)) return res;
   *dst = 0;
-  if (s21_is_zero(src)) {
-    res = OK;
-  } else if (src.bits[2] == 0) {
-    int sign = s21_get_sign(src);
-    int scale = s21_get_scale(&src);
-    unsigned int max_allowed = sign ? 2147483648U : 2147483647U;
-    unsigned long long mantissa = src.bits[0];
-    mantissa |= (unsigned long long)src.bits[1] << 32;
-    for (int i = 0; i < scale && mantissa > 0; i++) mantissa /= 10;
+  s21_decimal result;
+  s21_truncate(src, &result);
+  int sign = s21_get_sign(&result);
+  if (result.bits[1] == 0 && result.bits[2] == 0) {
+    unsigned mantissa = result.bits[0];
+    unsigned max_allowed = sign ? 2147483648U : 2147483647U;
     if (mantissa <= max_allowed) {
-      if (sign && mantissa == 2147483648U)
-        *dst = MIN_INT;
-      else
-        *dst = !sign ? (int)mantissa : -(int)mantissa;
+      if (sign) {
+        *dst = mantissa == 2147483648U ? MIN_INT : -(int)mantissa;
+      } else {
+        *dst = (int)mantissa;
+      }
       res = OK;
     }
   }
@@ -49,7 +48,7 @@ int s21_from_decimal_to_int(s21_decimal src, int* dst) {
 
 int s21_from_float_to_decimal(float src, s21_decimal* dst) {
   if (src < 0) dst->bits[3] |= 1u << 31;
-  
+
   return 0;
 }
 
@@ -77,36 +76,45 @@ int s21_from_int_to_decimal(int src, s21_decimal* dst) {
   return res;
 }
 
-
-s21_big_decimal s21_decimal_to_big_internal(s21_decimal* value) {
-    s21_big_decimal res = {0};
-    if (value) {
+s21_big_decimal* s21_decimal_to_big(s21_decimal* value) {
+  static s21_big_decimal res = {0};
+  if (value) {
     for (int i = 0; i < 3; ++i) res.bits[i] = value->bits[i];
     if (s21_get_sign(value)) res.sign = 1;
     res.scale = s21_get_scale(value);
-    }
-    return res;
+  }
+  return &res;
 }
 
+s21_big_decimal s21_decimal_to_big_internal(s21_decimal* value) {
+  s21_big_decimal res = {0};
+  if (value) {
+    for (int i = 0; i < 3; ++i) res.bits[i] = value->bits[i];
+    if (s21_get_sign(value)) res.sign = 1;
+    res.scale = s21_get_scale(value);
+  }
+  return res;
+}
 
-s21_decimal s21_big_to_decimal_internal(s21_big_decimal* big_result, s21_decimal* result) {
+s21_decimal s21_big_to_decimal_internal(s21_big_decimal* big_result,
+                                        s21_decimal* result) {
   if (!big_result || !result) {
     s21_decimal empty;
     s21_null_decimal(&empty);
     return empty;
   }
-  
+
   s21_null_decimal(result);
   for (int i = 0; i < 3; ++i) {
-    result -> bits[i] = big_result -> bits[i];
+    result->bits[i] = big_result->bits[i];
   }
 
-  
-  if(big_result->sign) {
+  if (big_result->sign) {
     result->bits[3] |= 1u << 31;
   }
-  // 0xFF это маска 11111111 (гарантирует, что при scale > 28 мы не выйдем за перделы диапазона 16 - 23 бит)
+  // 0xFF это маска 11111111 (гарантирует, что при scale > 28 мы не выйдем за
+  // перделы диапазона 16 - 23 бит)
   result->bits[3] |= (big_result->scale & 0xFF) << 16;
-  
+
   return *result;
 }
