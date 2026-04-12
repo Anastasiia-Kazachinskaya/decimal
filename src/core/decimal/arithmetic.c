2f334930ed5ef63bf21cb486e77dbb15b1757b8f
titanlig <kattyalex97@mail.ru>
2026-04-12 18:22:13 +0300

    save add_func

--- src/core/decimal/arithmetic.c
+++ src/core/decimal/arithmetic.c
@@ -10,21 +10,48 @@ int s21_mul(s21_decimal value_1, s21_decimal value_2, s21_decimal* result);
 
 int s21_div(s21_decimal value_1, s21_decimal value_2, s21_decimal* result);
 
-// int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal* result){
-//   s21_big_decimal* lhs = s21_decimal_to_big(&value_1);
-//   s21_big_decimal* rhs = s21_decimal_to_big(&value_2);
+int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal* result) {
+  if (!result) {
+    return ERROR;
+  }
 
-//   s21_big_decimal* big_result = s21_big_zero();
-//   int code = s21_big_decimal_add(lhs, rhs, big_result);
-//   free(lhs);
-//   free(rhs);
+  s21_null_decimal(result);
 
-//   s21_decimal another_result = s21_big_to_decimal(big_result);
-//   memcpy(result, &another_result, sizeof(s21_decimal));
+  s21_big_decimal big1 = s21_decimal_to_big_internal(&value_1);
+  s21_big_decimal big2 = s21_decimal_to_big_internal(&value_2);
 
-//   return code;
-// }
+  int sign1 = big1.sign;
+  int sign2 = big2.sign ? 0 : 1;
 
+  s21_normalize_big_pair(&big1, &big2);
+  s21_big_decimal res_big;
+  s21_null_big_decimal(&res_big);
+
+  int target_scale = big1.scale;
+  int code = s21_big_perform_signed_operation(sign1, sign2, &big1, &big2, &res_big);
+
+  if (code != OK) {
+    return CALCULATION_ERROR;
+  }
+
+  res_big.scale = target_scale;
+  int final_code = s21_handle_overflow_and_rounding(&res_big);
+  if (final_code == OK && res_big.scale == 0) {
+    int is_greater_than_max = check_mantissa(res_big);
+    if (is_greater_than_max) {
+      if (res_big.sign == 0) {
+        final_code = 1;
+      } else {
+        final_code = 2;
+      }
+    }
+  }
+  if (final_code == OK) {
+    s21_big_to_decimal_internal(&res_big, result);
+  }
+
+  return final_code;
+}
 
 int s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal* result) {
   if (!result) {
